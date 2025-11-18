96129e74cbf9bc76e08ff2d89f6e3367d42451af
jimmy <jimmyzheng8298@gmail.com>
2025-11-17 22:34:00 -0500

    raycast Collision solution added
    Instead of relying on unity's collision system I've used raycast to detect collision. The solution is much better than unity's collision system.

--- WALMART-BTD6/Assets/scripts/projParent.cs
+++ WALMART-BTD6/Assets/scripts/projParent.cs
@@ -21,9 +21,14 @@ public class projectileParentForStraightLinearProj : MonoBehaviour, IProjctileOw
     protected float projSpeed;
     protected float lifespan = 3;
 
+    
+
     protected bool mutipleOverLappingCollider = false;
     protected bool isDead =false;
     protected List<GameObject> listOfGO = new List<GameObject>();
+
+    protected Vector3 lastPoistion;
+
 
     protected void Start()
     {
@@ -40,16 +45,18 @@ public class projectileParentForStraightLinearProj : MonoBehaviour, IProjctileOw
         
         if (orgEnemyPosition != null)
         {
-            //cast a ray up to the future position and if it collides with anything do stuff ig
+            lastPoistion = gameObject.transform.position;
             transform.Translate(new Vector3(0, orgEnemyPosition.y * 5 * Time.deltaTime * projSpeed, 0));
+            safetyCheckForCollisionBackWards();
+            safetyCheckForCollisionForward();
         }
-        if (mutipleOverLappingCollider && !isDead) {
-            for (int i = 0; i < pierce; i++) {
-                listOfGO[i].GetComponent<IDamageTaken>().damageTaken(damage);
-                isDead = true;
-            }
-            Destroy(gameObject);
-        }
+        //if (mutipleOverLappingCollider && !isDead) {
+        //    for (int i = 0; i < pierce; i++) {
+        //        listOfGO[i].GetComponent<IDamageTaken>().damageTaken(damage);
+        //        isDead = true;
+        //    }
+        //    Destroy(gameObject);
+        //}
     }
 
     protected IEnumerator selfDest()
@@ -57,37 +64,89 @@ public class projectileParentForStraightLinearProj : MonoBehaviour, IProjctileOw
         yield return new WaitForSeconds(lifespan);
         Destroy(gameObject);
     }
-
-    protected void OnTriggerEnter(Collider other)
-    {
-        listOfGO = new List<GameObject>();
-        IDamageTaken enemyDamage = other.GetComponent<IDamageTaken>();
-        if (enemyDamage != null && other.gameObject.tag == "enemy")
-        {
-            //ok so added some lines of code to account for 5 collision in the same frame
-            //adds the gameobject onto a list
+    //for mutiple collision during a frame going over an projectile priece limit we can do raycast but get an array back limiting
+    //protected void OnTriggerEnter(Collider other)
+    //{
+    //    listOfGO = new List<GameObject>();
+    //    IDamageTaken enemyDamage = other.GetComponent<IDamageTaken>();
+    //    if (enemyDamage != null && other.gameObject.tag == "enemy")
+    //    {
+    //        //ok so added some lines of code to account for 5 collision in the same frame
+    //        //adds the gameobject onto a list
             
-            listOfGO.Add(other.gameObject);
+    //        listOfGO.Add(other.gameObject);
            
-            //isDead is here because destory only gets rid of the GameObject at end of frame
-            //this works lmao
-            if (listOfGO.Count>pierce)
-            {
-                mutipleOverLappingCollider = true;
-            }
-            if (isDead == false) 
-            {
-                enemyDamage.damageTaken(damage);
-                owner.GetComponent<IPopToPopCount>().damageDealt(1);
-                pierce--;
-            }
+    //        //isDead is here because destory only gets rid of the GameObject at end of frame
+    //        //this works lmao
+    //        if (listOfGO.Count>pierce)
+    //        {
+    //            mutipleOverLappingCollider = true;
+    //        }
+    //        if (isDead == false) 
+    //        {
+    //            enemyDamage.damageTaken(damage);
+    //            owner.GetComponent<IPopToPopCount>().damageDealt(1);
+    //            pierce--;
+    //        }
 
-            if (pierce == 0)
+    //        if (pierce == 0)
+    //        {
+    //            isDead = true;
+    //            Destroy(gameObject);
+    //        }
+    //    }
+    //}
+    protected void safetyCheckForCollisionBackWards() {
+        RaycastHit[] hit;
+        if (lastPoistion != null) {
+            Debug.DrawRay(gameObject.transform.position, -Vector3.Normalize(gameObject.transform.position - lastPoistion), Color.rebeccaPurple,0.5f);
+            Debug.Log(-gameObject.transform.forward);
+            Debug.Log(Vector3.Normalize(gameObject.transform.position - lastPoistion));
+            hit = Physics.RaycastAll(gameObject.transform.position, -Vector3.Normalize(gameObject.transform.position-lastPoistion),Vector3.Magnitude(gameObject.transform.position - lastPoistion),1<<9);
+            if (hit.Length > 0) {
+                for (int i = 0; i < (int)pierce; i++)
+                {
+                    if (isDead == false && i<hit.Length)
+                    {
+                        Debug.Log(hit[i].collider.gameObject);
+                        hit[i].collider.gameObject.GetComponent<IDamageTaken>().damageTaken(damage);
+                        pierce--;
+                    }
+                    if (pierce == 0)
+                    {
+                        Destroy(gameObject);
+                        isDead = true;
+                    }
+                }            
+            }              
+        }
+    }
+    protected void safetyCheckForCollisionForward()
+    {
+        RaycastHit[] hit;
+        if (!isDead)
+        {
+            Debug.DrawRay(gameObject.transform.position, Vector3.Normalize(gameObject.transform.position - lastPoistion), Color.rebeccaPurple, 0.5f);
+            hit = Physics.RaycastAll(gameObject.transform.position, Vector3.Normalize(gameObject.transform.position - lastPoistion), gameObject.transform.localScale.y, 1 << 9);
+            if (hit.Length > 0)
             {
-                isDead = true;
-                Destroy(gameObject);
+                for (int i = 0; i < (int)pierce; i++)
+                {
+                    if (isDead == false && i < hit.Length)
+                    {
+                        Debug.Log(hit[i].collider.gameObject);
+                        hit[i].collider.gameObject.GetComponent<IDamageTaken>().damageTaken(damage);
+                        pierce--;
+                    }
+                    if (pierce == 0)
+                    {
+                        Destroy(gameObject);
+                        isDead = true;
+                    }
+                }
             }
         }
+            
     }
     public void setProjectileOwner(GameObject trackstar)
     {
